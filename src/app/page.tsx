2526cc8c8d4f7af34c42fd8be0643cb9fc1a429f
zeriong <jaeryong95@gmail.com>
2025-01-05 20:36:57 +0900

    feat: 임시 렌더링 적용 후 테스트 진행

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,43 @@
-export default function Home() {
-  return (
-    <div className="w-full">
-      <p>test threeJS in react</p>
-      <section></section>
-    </div>
-  );
+"use client";
+
+import React, { useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, TransformControls, useTexture } from "@react-three/drei";
+
+interface Props {
+  svgPath: string;
 }
+
+const DraggableSVG = ({ svgPath }: Props) => {
+  const meshRef = useRef(null);
+  const texture = useTexture(svgPath);
+  const [isDragging, setIsDragging] = useState(false);
+
+  return (
+    <TransformControls
+      object={meshRef.current}
+      enabled={isDragging}
+      onMouseDown={() => setIsDragging(true)}
+      onMouseUp={() => setIsDragging(false)}
+    >
+      <mesh ref={meshRef} position={[0, 0, 0]}>
+        <planeGeometry args={[5, 5]} />
+        <meshBasicMaterial map={texture} transparent={true} />
+      </mesh>
+    </TransformControls>
+  );
+};
+
+const App = () => {
+  return (
+    <Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <OrbitControls />
+      {/* Draggable SVG */}
+      <DraggableSVG svgPath="/svg/japan_map.svg" />
+    </Canvas>
+  );
+};
+
+export default App;

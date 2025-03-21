7d9f3a4b59b85017b2a0f0b763426c2315d6b9f2
zeriong <jaeryong95@gmail.com>
2025-03-21 20:41:45 +0900

    refactor: aside 리팩토링 진행중... 20%

--- src/components/layout/MainAside.tsx
+++ src/components/layout/MainAside.tsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function MainAside() {
   return (
     <aside className={"max-w-[360px] w-full h-full shadow-2xl border-r border-gray-300"}>
-      <ul>
+      <ul className={"p-[16px] flex flex-col gap-[16px]"}>
+        <li>
+          <p>ThreeJS</p>
+        </li>
+
         <li>
           <p>ThreeJS</p>
         </li>

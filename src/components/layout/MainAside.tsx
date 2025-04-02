7afa4b4ec2c76a9bd79f8ab967bc9511803aa001
zeriong <jaeryong95@gmail.com>
2025-04-03 01:17:09 +0900

    style: aside 링크 방식 및 디자인 구상중...

--- src/components/layout/MainAside.tsx
+++ src/components/layout/MainAside.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
 export default function MainAside() {
   return (
     <aside className={"max-w-[360px] w-full h-full shadow-2xl border-r border-gray-300"}>
       <ul className={"p-[16px] flex flex-col gap-[16px]"}>
-        <li>
-          <p>ThreeJS</p>
+        <li className={"p-4 font-bold text-[24px]"}>
+          <Link href={"/three/r3f/map"}>ThreeJS Map</Link>
         </li>
 
         <li>

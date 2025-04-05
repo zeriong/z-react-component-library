51c6c5c80476533e1573f4bde19b3d2b630d0539
zeriong <jaeryong95@gmail.com>
2025-04-05 18:50:40 +0900

    refactor: aside list depth를 대비한 type 추가 및 작업

--- src/constants/aside.ts
+++ src/constants/aside.ts
@@ -1,7 +1,16 @@
-export interface IAsideListItem {
+interface IAsideItemCore {
   id: string;
   name: string;
+}
+
+interface IAsideListChildrenItem extends IAsideItemCore {
   href: string;
 }
 
-export const ASIDE_LIST: IAsideListItem[] = [{ id: "aside_1", name: "ThreeJS Map", href: "/three/r3f/map" }];
+export interface IAsideListItem extends IAsideItemCore {
+  children?: IAsideListChildrenItem[];
+}
+
+export const ASIDE_LIST: IAsideListItem[] = [
+  { id: "aside_1", name: "ThreeJS", children: [{ id: "aside_map", name: "R3F Map", href: "/three/r3f/map" }] },
+];

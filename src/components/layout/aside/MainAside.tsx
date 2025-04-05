619a463cc7f0c1e54d1eaa69b0c7ecd2687b1a32
zeriong <jaeryong95@gmail.com>
2025-04-05 18:19:26 +0900

    design: aside bar item 스타일 변경

--- src/components/layout/aside/MainAside.tsx
+++ src/components/layout/aside/MainAside.tsx
@@ -5,7 +5,11 @@ export default function MainAside() {
   return (
     <aside className={"max-w-[360px] w-full h-full shadow-2xl border-r border-gray-300"}>
       <ul className={"p-[16px] flex flex-col gap-[16px]"}>
-        <li className={"p-4 font-bold text-[24px]"}>
+        <li
+          className={
+            "py-4 px-[20px] font-bold text-[24px] transition-all duration-200 cursor-pointer rounded-md hover:bg-gray-100"
+          }
+        >
           <Link href={"/three/r3f/map"}>ThreeJS Map</Link>
         </li>
 

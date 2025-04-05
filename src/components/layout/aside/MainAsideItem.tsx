3ef440a3327833f86349eda8b411763ba2fcf33e
zeriong <jaeryong95@gmail.com>
2025-04-05 18:52:37 +0900

    refactor: aside list item depth 대응 설계중...

--- src/components/layout/aside/MainAsideItem.tsx
+++ src/components/layout/aside/MainAsideItem.tsx
@@ -1,20 +1,16 @@
-import Link from "next/link";
 import React from "react";
 import { IAsideListItem } from "@/constants/aside";
 
 export default function MainAsideItem({ listItem }: Props) {
-  const { name, href } = listItem;
+  const { name } = listItem;
 
   return (
-    <li className={"w-full"}>
-      <Link
-        className={
-          "py-4 px-[20px] w-full block font-bold text-[24px] transition-all duration-200 cursor-pointer rounded-md hover:bg-gray-100"
-        }
-        href={href}
-      >
-        {name}
-      </Link>
+    <li
+      className={
+        "w-full py-4 px-[20px] block font-bold text-[24px] transition-all duration-200 cursor-pointer rounded-md hover:bg-gray-100"
+      }
+    >
+      {name}
     </li>
   );
 }

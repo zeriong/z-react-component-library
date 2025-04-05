af74c6f86bfb966d64bd3f968b61def539c3fd9d
zeriong <jaeryong95@gmail.com>
2025-04-05 18:41:19 +0900

    refactor: aside item style 정상화

--- src/components/layout/aside/MainAsideItem.tsx
+++ src/components/layout/aside/MainAsideItem.tsx
@@ -2,16 +2,23 @@ import Link from "next/link";
 import React from "react";
 import { IAsideListItem } from "@/constants/aside";
 
-export default function MainAsideItem(props: IAsideListItem) {
-  const { name, href } = props;
+export default function MainAsideItem({ listItem }: Props) {
+  const { name, href } = listItem;
 
   return (
-    <li
-      className={
-        "py-4 px-[20px] font-bold text-[24px] transition-all duration-200 cursor-pointer rounded-md hover:bg-gray-100"
-      }
-    >
-      <Link href={href}>{name}</Link>
+    <li className={"w-full"}>
+      <Link
+        className={
+          "py-4 px-[20px] w-full block font-bold text-[24px] transition-all duration-200 cursor-pointer rounded-md hover:bg-gray-100"
+        }
+        href={href}
+      >
+        {name}
+      </Link>
     </li>
   );
+}
+
+interface Props {
+  listItem: IAsideListItem;
 }

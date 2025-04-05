0201c6fedf1af57841c0eeaf2ed8bc797d5e261b
zeriong <jaeryong95@gmail.com>
2025-04-05 18:41:38 +0900

    refactor: aside list map을 활용 및 적용

--- src/components/layout/aside/MainAside.tsx
+++ src/components/layout/aside/MainAside.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import { ASIDE_LIST } from "@/constants/aside";
+import MainAsideItem from "@/components/layout/aside/MainAsideItem";
 
 export default function MainAside() {
   return (
     <aside className={"max-w-[360px] w-full h-full shadow-2xl border-r border-gray-300"}>
       <ul className={"p-[16px] flex flex-col gap-[16px]"}>
-        <li
-          className={
-            "py-4 px-[20px] font-bold text-[24px] transition-all duration-200 cursor-pointer rounded-md hover:bg-gray-100"
-          }
-        >
-          <Link href={"/three/r3f/map"}>ThreeJS Map</Link>
-        </li>
+        {ASIDE_LIST.map((item) => {
+          return <MainAsideItem key={item.id} listItem={item} />;
+        })}
 
         <li>
           <p>ThreeJS</p>

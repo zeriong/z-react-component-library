767b327cdd18dc6e28e0937807b209b1be51cca4
zeriong <jaeryong95@gmail.com>
2025-04-05 18:40:41 +0900

    refactor: threeJS map component aside list item에 정상 적용

--- src/constants/aside.ts
+++ src/constants/aside.ts
@@ -4,4 +4,4 @@ export interface IAsideListItem {
   href: string;
 }
 
-export const ASIDE_LIST: IAsideListItem[] = [{ id: "string", name: "string", href: "string" }];
+export const ASIDE_LIST: IAsideListItem[] = [{ id: "aside_1", name: "ThreeJS Map", href: "/three/r3f/map" }];

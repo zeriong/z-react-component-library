095a86d6ffe59ef58f743ebd349f336f978e5d64
zeriong <jaeryong95@gmail.com>
2025-04-11 10:49:37 +0900

    feat: next에서 활용 가능한 query string handler hook 추가

--- src/app/hooks/useHandleParams/page.tsx
+++ src/app/hooks/useHandleParams/page.tsx
@@ -1,3 +1,50 @@
-export default function Page() {
-  return <div>useHandleParams</div>;
+// app/hooks/useQueryString.ts
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+interface Props {
+  isReplace: boolean;
 }
+export const useQueryString = ({ isReplace }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  function routerAction(urlString: string) {
+    if (isReplace) router.replace(urlString);
+    else router.push(urlString);
+  }
+
+  const getParam = (key: string) => {
+    return routerAction(key);
+  };
+
+  const setParam = (key: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(key, value);
+    routerAction(`${pathname}?${params.toString()}`);
+  };
+
+  const deleteParam = (key: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete(key);
+
+    const queryStr = params.toString();
+    const url = queryStr ? `${pathname}?${queryStr}` : pathname;
+
+    routerAction(url);
+  };
+
+  const clearAllParams = () => {
+    routerAction(pathname);
+  };
+
+  return {
+    getParam,
+    setParam,
+    deleteParam,
+    clearAllParams,
+    allParams: searchParams,
+  };
+};
--- src/components/layout/aside/MainAsideItem.tsx
+++ src/components/layout/aside/MainAsideItem.tsx
@@ -1,20 +1,41 @@
+"use client";
+
 import React from "react";
 import { IAsideListItem } from "@/constants/aside";
+import MainAsideInnerItem from "@/components/layout/aside/MainAsideInnerItem";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface Props {
   listItem: IAsideListItem;
 }
 
 export default function MainAsideItem({ listItem }: Props) {
-  const { name } = listItem;
+  const { name, children } = listItem;
+
+  const router = useRouter();
+
+  function handleAccordion() {
+    router.replace();
+  }
 
   return (
-    <li
-      className={
-        "w-full py-4 px-[20px] block font-bold text-[24px] transition-all duration-200 cursor-pointer rounded-md hover:bg-gray-100"
-      }
-    >
-      {name}
+    <li>
+      <button
+        type={"button"}
+        className={
+          "text-start w-full py-4 px-[20px] block font-bold text-[24px] transition-all duration-200 cursor-pointer rounded-md hover:bg-sky-100"
+        }
+        onClick={() => handleAccordion()}
+      >
+        {name}
+      </button>
+      {children?.length && (
+        <ul className={"text-[18px] p-[12px] bg-gray-100 rounded-sm"}>
+          {children.map((item) => {
+            return <MainAsideInnerItem key={item.id} item={item} />;
+          })}
+        </ul>
+      )}
     </li>
   );
 }

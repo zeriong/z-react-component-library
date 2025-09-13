58f8d9252c2f890e99315dcce24f7f430a87e33d
zeriong <jaeryong95@gmail.com>
2025-09-13 18:26:08 +0900

    refactor: vitest + vite관련 설정 및 패키지 정리하여 온전한 nextjs버전으로 컨버팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,13 +12,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-    }
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript"), {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
   }
-];
+}, ...storybook.configs["flat/recommended"]];
 
 export default eslintConfig;
